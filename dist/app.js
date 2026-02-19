8d9e1f75d955e9f45201c74ea2f347ec347092b7
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2026-02-19 13:13:55 +0600

    feat: added user model

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,13 @@
+import cors from "cors";
 import express from "express";
+import { globalError } from "./app/middleware/globalError";
+import { notFound } from "./app/middleware/notFound";
 const app = express();
+app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Courstack running...");
 });
+app.use(notFound);
+app.use(globalError);
 export default app;
--- src/app/middleware/globalError.ts
+++ src/app/middleware/globalError.ts
@@ -1,9 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
+import sendResponse from "../utils/SendResponse";
 
 export const globalError = (error: unknown, req: Request, res: Response, _next: NextFunction) => {
   const err = error
   const statusCode = 500
 
-  res.status(statusCode).json(err)
+  sendResponse(res, {
+    statusCode,
+    success: false,
+    message: "Something want wrong!",
+    data: err,
+  })
 }
